f5ad9fd428aead8e596782b241d3ecad120177ec
Gustavo <chdrlz@gmail.com>
2020-06-17 01:47:24 -0300

    Created the initial structure of the GameGenerationDetails component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 import Nav from './components/Nav'
+import GameGenerationDetails from './components/GameGenerationDetails'
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1 className="text-center">POKEMON</h1>
       <p className="text-center">Select the Pokémon game generation you want to view more details</p>
       <Nav/>
+      <GameGenerationDetails/>
 
     </div>
   );
--- src/components/GameGenerationDetails.jsx
+++ src/components/GameGenerationDetails.jsx
@@ -1,1 +1,117 @@
+import React from 'react'
 
+export default function GameGenerationDetails() {
+
+  return (
+    <div className="border">
+      <ul>
+        <li>Name: </li>
+        <li>Main Region: </li>
+        <li>
+          Version Groups:
+          <ul>
+            <li>Red-Blue</li>
+            <li>Yellow</li>
+          </ul>
+        </li>
+        <li>
+          Pokémon Types:
+          <ul>
+            <li>Type 1</li>
+            <li>Type 2</li>
+          </ul>
+        </li>
+        <li>
+          Pokémon Species:
+          <ul className="pokemon-species d-flex flex-wrap p-0">
+            <li>
+              <a href="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png" alt=""/> 
+                Type 1
+              </a>
+            </li>
+            <li>
+              <a href="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png" alt=""/> 
+                Type 1
+              </a>
+            </li>
+            <li>
+              <a href="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png" alt=""/> 
+                Type 1
+              </a>
+            </li>
+            <li>
+              <a href="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png" alt=""/> 
+                Type 1
+              </a>
+            </li>
+            <li>
+              <a href="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png" alt=""/> 
+                Type 1
+              </a>
+            </li>
+            <li>
+              <a href="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png" alt=""/> 
+                Type 1
+              </a>
+            </li>
+            <li>
+              <a href="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png" alt=""/> 
+                Type 1
+              </a>
+            </li>
+            <li>
+              <a href="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png" alt=""/> 
+                Type 1
+              </a>
+            </li>
+            <li>
+              <a href="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png" alt=""/> 
+                Type 1
+              </a>
+            </li>
+            <li>
+              <a href="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png" alt=""/> 
+                Type 1
+              </a>
+            </li>
+            <li>
+              <a href="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png" alt=""/> 
+                Type 1
+              </a>
+            </li>
+            <li>
+              <a href="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png" alt=""/> 
+                Type 1
+              </a>
+            </li>
+            <li>
+              <a href="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png" alt=""/> 
+                Type 1
+              </a>
+            </li>
+            <li>
+              <a href="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/413.png" alt=""/> 
+                Type 1
+              </a>
+            </li>
+            
+          </ul>
+        </li>
+      </ul>
+    </div>
+  )
+}
