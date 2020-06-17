0a60fadbe01d9a0f09b84e75e9e1c00b8b0d5d87
Gustavo <chdrlz@gmail.com>
2020-06-17 16:49:37 -0300

    Added Pokemon Logo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { initialState } from './state/InitialState';
 import { reducer } from './state/Reducer';
 import Nav from './components/Nav'
 import GameGenerationDetails from './components/GameGenerationDetails'
+import pokemonLogo from './img/pokemon.png'
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
@@ -11,7 +12,7 @@ function App() {
   return (
     <StateProvider initialState={initialState} reducer={reducer}>
       <div className="container">
-        <h1 className="text-center">POKEMON</h1>
+        <img className="d-block mx-auto mt-3 mb-5" src={pokemonLogo} alt="Pokémon"/>
         <p className="text-center">Select the Pokémon game generation you want to view more details</p>
         <Nav/>
         <GameGenerationDetails/>
