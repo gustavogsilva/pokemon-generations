5de16667ba132cbd3848182e5b1a7905c06c188f
Gustavo Silva <chdrlz@gmail.com>
2020-09-07 23:34:20 -0300

    Updates

---
+++ precache-manifest.184f86fc96d63744a73b7a10d5ff2227.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f0d3f7faced40d3321664ceda5740b",
+    "url": "/pokemon-generations/index.html"
+  },
+  {
+    "revision": "7824c68a230b2e19608e",
+    "url": "/pokemon-generations/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c44969fbfc1055fcc3e0",
+    "url": "/pokemon-generations/static/css/main.75240269.chunk.css"
+  },
+  {
+    "revision": "7824c68a230b2e19608e",
+    "url": "/pokemon-generations/static/js/2.40246a47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon-generations/static/js/2.40246a47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c44969fbfc1055fcc3e0",
+    "url": "/pokemon-generations/static/js/main.6ae2c0a0.chunk.js"
+  },
+  {
+    "revision": "f577a4c908dbcf53446c",
+    "url": "/pokemon-generations/static/js/runtime-main.cd497efd.js"
+  },
+  {
+    "revision": "d68ddf54ff89aad1f6077717edc904aa",
+    "url": "/pokemon-generations/static/media/pokemon.d68ddf54.png"
+  }
+]);
